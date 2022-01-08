097132999b92682b03dfb39b4e60b6eedde3fb07
Rahul-D78 <rahulparida933@gmail.com>
2022-01-09 00:04:48 +0530

    FEAT: Decorator  pattern init
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>

---
+++ src/main/java/decorator/Expresso.java
@@ -0,0 +1,10 @@
+package decorator;
+
+public class Expresso extends Beverage {
+    public Expresso() {
+        description = "Expresso";
+    }
+    public double cost() {
+        return 1.99;
+    }
+}
---
+++ src/main/java/decorator/HouseBlend.java
@@ -0,0 +1,10 @@
+package decorator;
+
+public class HouseBlend extends Beverage {
+    public HouseBlend() {
+        description = "I am HouseBlend";
+    }
+    public double cost() {
+        return .89;
+    }
+}
